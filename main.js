4eb9ddf3126519b8ffe52daafb50129e2707aa1f
Tryy-43 <165661078+Tryy-43@users.noreply.github.com>
2024-08-07 13:18:14 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ const btnShow = document.querySelector("#btn-show");
 const sideMenu = document.querySelector("#side");
 const btnHide = document.querySelector("#btn-hide");
 btnShow.addEventListener("click", () => {
-  sideMenu.classList.remove("w-0");
-  sideMenu.classList.add("w-1/2");
+  sideMenu.classList.remove("hidden");
+  sideMenu.classList.add("flex");
 });
 btnHide.addEventListener("click", () => {
-  sideMenu.classList.remove("w-1/2");
-  sideMenu.classList.add("w-0");
+  sideMenu.classList.remove("flex");
+  sideMenu.classList.add("hidden");
 });
 // const menuLink = document.querySelectorAll(".nav-link");
 // menuLink.forEach((e) => {
@@ -18,27 +18,27 @@ btnHide.addEventListener("click", () => {
 const allLinke = document.querySelectorAll("#side > li");
 allLinke.forEach((e) => {
   e.addEventListener("click", () => {
-    sideMenu.classList.remove("w-1/2");
-    sideMenu.classList.add("w-0");
+    sideMenu.classList.remove("flex");
+    sideMenu.classList.add("hidden");
   });
 });
 
-let sections = document.querySelectorAll("section");
-let navlinks = document.querySelectorAll("#nav li a");
-Window.onscroll = () => {
-  sections.forEach((sec) => {
-    let top = window.screenY;
-    let offset = sec.offsetTop;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
+// let sections = document.querySelectorAll("section");
+// let navlinks = document.querySelectorAll("#nav li a");
+// Window.onscroll = () => {
+//   sections.forEach((sec) => {
+//     let top = window.screenY;
+//     let offset = sec.offsetTop;
+//     let height = sec.offsetHeight;
+//     let id = sec.getAttribute("id");
 
-    if (top >= offset && top < offset + height) {
-      navlinks.forEach((links) => {
-        links.classList.remove("activeee");
-        document
-          .querySelector("#nav li a [herf*= " + id + "]")
-          .classList.add("activeee");
-      });
-    }
-  });
-};
+//     if (top >= offset && top < offset + height) {
+//       navlinks.forEach((links) => {
+//         links.classList.remove("activeee");
+//         document
+//           .querySelector("#nav li a [herf*= " + id + "]")
+//           .classList.add("activeee");
+//       });
+//     }
+//   });
+// };
